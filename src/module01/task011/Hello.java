7b2df7ca850a81fc46d223218ce49bdd6d368d2d
igorlazniuk <igorlazniuk@gmail.com>
2017-04-14 23:28:53 +0300

    HomeWork1

--- src/module01/task011/Hello.java
+++ src/module01/task011/Hello.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 /**
  * Created by IGOR.LAZNIUK on 14.04.2017.
+
+ Создать консольное приложение, которое запрашивает имя пользователя. После ввода имени выводит строку: Hello, <введённое имя>
+ Пример:
+ Enter Your name:
+ Eugene
+ Hello, Eugene
  */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Enter Your name: ");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, " + scanner.next());
+        //сканнер закрой
+        scanner.close();
 
     }
 }
--- src/module01/task012/Arrays.java
+++ src/module01/task012/Arrays.java
@@ -2,6 +2,19 @@ package module01.task012;
 
 /**
  * Created by IGOR.LAZNIUK on 14.04.2017.
+ *
+ Создать консольное приложение, которое просит пользователя ввести из консоли 5 чисел
+ Все числа должны быть записаны в массив целых чисел.
+ После этого, массив должен быть отсортирован по возрастанию и выведен консоль.
+ Сначала начальная версия, далее - отсортированная.
+ Пример:
+ Enter number 1:5
+ Enter number 2:4
+ Enter number 3: 3
+ Enter number 4: 2
+ Enter number 5: 1
+ Initial array: 5, 4, 3, 2, 1
+ Sorted array: 1, 2, 3, 4, 5.
  */
 
 import java.util.ArrayList;
@@ -12,13 +25,14 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
+        //Когда используешь класс обертку в коллекции, указывай явно её при инициализации в дженерике
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
         for (int i = 1; i <= 5; i++) {
             System.out.print("Enter Number " + i + " of 5: ");
             list.add(scanner.nextInt());
-
         }
+        //сканнер закрой
         scanner.close();
         System.out.println("Initial array: " + list.toString());
         Collections.sort(list);
