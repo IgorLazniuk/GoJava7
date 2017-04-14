5584cf49daffaa49aa129cfcdaa5e28cb5f806e1
igorlazniuk <igorlazniuk@gmail.com>
2017-04-14 13:17:24 +0300

    HomeWork1

---
+++ src/module01/task03/Hello.java
@@ -0,0 +1,15 @@
+package module01.task03;
+
+import java.util.Scanner;
+
+/**
+ * Created by IGOR.LAZNIUK on 14.04.2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Enter Your name: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, " + scanner.next());
+
+    }
+}
