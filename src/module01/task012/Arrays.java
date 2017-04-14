7592456e8cce212ccda36c6d7340ca615639511d
igorlazniuk <igorlazniuk@gmail.com>
2017-04-14 17:40:55 +0300

    HomeWork1

--- src/module01/task012/Arrays.java
+++ src/module01/task012/Arrays.java
@@ -12,13 +12,14 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 1; i <= 5; i++) {
             System.out.print("Enter Number " + i + " of 5: ");
             list.add(scanner.nextInt());
 
         }
+        scanner.close();
         System.out.println("Initial array: " + list.toString());
         Collections.sort(list);
         System.out.println("Sorted array: " + list.toString());
