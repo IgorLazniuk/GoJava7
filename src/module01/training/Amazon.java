22f7f590730be58c3419a889c7375412b407bf6f
igorlazniuk <igorlazniuk@gmail.com>
2017-04-19 17:58:10 +0300

    Amazon

--- src/module01/training/Amazon.java
+++ src/module01/training/Amazon.java
@@ -6,7 +6,7 @@ package module01.training;
  */
 public class Amazon {
     public static void main(String[] args) {
-        System.out.println(urlModifier(13, "https://www.amazon.com/s/ref=sr_pg_3?fst=p90x%3A1&rh=i%3Aaps%2Ck%3Aumbrella&page=777&keywords=umbrella&ie=UTF8&qid=1492586698&spIA=B06Y3265XF,B00UR0EFEK,B01J4UCSFG,B01NAYOAXE"));
+        System.out.println(urlModifier(10, "https://www.amazon.com/s/ref=sr_pg_3?fst=p90x%3A1&rh=i%3Aaps%2Ck%3Aumbrella&page=3&keywords=umbrella&ie=UTF8&qid=1492586698&spIA=B06Y3265XF,B00UR0EFEK,B01J4UCSFG,B01NAYOAXE"));
     }
 
     static String urlModifier(int page, String url) {
