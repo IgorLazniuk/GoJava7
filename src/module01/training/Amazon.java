1973171d155f60276d653eafa7e4ec0ffe692a1e
igorlazniuk <igorlazniuk@gmail.com>
2017-04-19 17:55:15 +0300

    Amazon

---
+++ src/module01/training/Amazon.java
@@ -0,0 +1,15 @@
+package module01.training;
+
+
+/**
+ * Created by IGOR.LAZNIUK on 19.04.2017.
+ */
+public class Amazon {
+    public static void main(String[] args) {
+        System.out.println(urlModifier(13, "https://www.amazon.com/s/ref=sr_pg_3?fst=p90x%3A1&rh=i%3Aaps%2Ck%3Aumbrella&page=777&keywords=umbrella&ie=UTF8&qid=1492586698&spIA=B06Y3265XF,B00UR0EFEK,B01J4UCSFG,B01NAYOAXE"));
+    }
+
+    static String urlModifier(int page, String url) {
+        return url.replaceFirst("page=[0-9]*", "page=" + page);
+    }
+}
